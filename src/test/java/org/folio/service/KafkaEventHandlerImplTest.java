8c672a71c7ea700e616e65d5148b0e1a13342631
MagzhanArtykov <magzhan_artykov@epam.com>
2024-01-31 16:25:43 +0600

    MODTLR-13 Handle request update event from data tenant to update ECS TLR

---
+++ src/test/java/org/folio/service/KafkaEventHandlerImplTest.java
@@ -0,0 +1,42 @@
+package org.folio.service;
+
+import org.folio.api.BaseIT;
+import org.folio.listener.kafka.KafkaEventListener;
+import org.folio.repository.EcsTlrRepository;
+import org.folio.service.impl.EcsTlrServiceImpl;
+import org.folio.service.impl.KafkaEventHandlerImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.Optional;
+import static org.folio.support.MockDataUtils.getEcsTlrEntity;
+import static org.folio.support.MockDataUtils.getMockDataAsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class KafkaEventHandlerImplTest extends BaseIT {
+  private static final String CHECK_IN_EVENT_SAMPLE = getMockDataAsString("mockdata/kafka/check_in.json");
+
+  @InjectMocks
+  private KafkaEventHandlerImpl eventHandler;
+
+  @InjectMocks
+  private EcsTlrServiceImpl ecsTlrService;
+
+  @MockBean
+  private EcsTlrRepository ecsTlrRepository;
+
+  @Autowired
+  private KafkaEventListener eventListener;
+
+  @Test
+  void handleCheckInEventInPickupFromOpenToAwaitingPickupTest() {
+    when(ecsTlrRepository.findByTlrId(any())).thenReturn(Optional.of(getEcsTlrEntity()));
+    eventListener.handleRequestEvent(CHECK_IN_EVENT_SAMPLE);
+    verify(ecsTlrRepository).save(any());
+  }
+}
---
+++ src/test/java/org/folio/support/MockDataUtils.java
@@ -0,0 +1,39 @@
+package org.folio.support;
+
+import lombok.SneakyThrows;
+import org.folio.EcsTlrApplication;
+import org.folio.domain.entity.EcsTlrEntity;
+import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+public class MockDataUtils {
+
+  public static final UUID TLR_ID = UUID.fromString("100d10bf-2f06-4aa0-be15-0b95b2d9f9e3");
+
+  public static EcsTlrEntity getEcsTlrEntity() {
+    return EcsTlrEntity.builder()
+      .id(UUID.randomUUID())
+      .tlrId(TLR_ID)
+      .build();
+  }
+
+  @SneakyThrows
+  public static String getMockDataAsString(String path) {
+    try (InputStream resourceAsStream = EcsTlrApplication.class.getClassLoader().getResourceAsStream(path)) {
+      if (resourceAsStream != null) {
+        return IOUtils.toString(resourceAsStream, StandardCharsets.UTF_8);
+      } else {
+        StringBuilder sb = new StringBuilder();
+        try (Stream<String> lines = Files.lines(Paths.get(path))) {
+          lines.forEach(sb::append);
+        }
+        return sb.toString();
+      }
+    }
+  }
+}
