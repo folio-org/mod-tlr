9d7628e4ae298141c2c310e34d921c5d7d1923d1
Magzhan <magzhan.artykov@gmail.com>
2024-02-08 17:38:36 +0600

    Update src/main/java/org/folio/repository/EcsTlrRepository.java
    Co-authored-by: OleksandrVidinieiev <56632770+OleksandrVidinieiev@users.noreply.github.com>

--- src/main/java/org/folio/repository/EcsTlrRepository.java
+++ src/main/java/org/folio/repository/EcsTlrRepository.java
@@ -9,5 +9,5 @@ import java.util.UUID;
 
 @Repository
 public interface EcsTlrRepository extends JpaRepository<EcsTlrEntity, UUID> {
-  Optional<EcsTlrEntity> findBySecondaryTlrId(UUID secondaryTlrId);
+  Optional<EcsTlrEntity> findBySecondaryRequestId(UUID secondaryRequestId);
 }
