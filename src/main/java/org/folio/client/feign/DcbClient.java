65893efd9fc24d5dca60fe3ed6ea3f30ca4edc2e
Alexander Kurash <alexander.kurash@gmail.com>
2024-03-01 20:28:57 +0200

    MODTLR-19 Create a client to update DCB transaction statuses (#16)
    * MODTLR-12 Initial implementation
    
    * MODTLR-12 Rename endpoint to be consistent
    
    * MODTLR-12 Rename copied schemas
    
    * MODTLR-19 Add schema, client
    
    * MODTLR-12 Rename component in yaml
    
    * MODTLR-19 Add schema, mapping
    
    * MODTLR-19 Add GET mapping for transaction status
    
    * MODTLR-12 Fix code smells in unit tests
    
    * MODTLR-19 Fix code smells in unit tests
    
    * MODTLR-19 Change schema, response type
    
    * MODTLR-12 Change schema, response type
    
    * MODTLR-12 Add transaction ID
    
    ---------
    
    Co-authored-by: Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>

--- src/main/java/org/folio/client/feign/DcbClient.java
+++ src/main/java/org/folio/client/feign/DcbClient.java
@@ -1,11 +1,14 @@
 package org.folio.client.feign;
 
 import org.folio.domain.dto.DcbTransaction;
+import org.folio.domain.dto.TransactionStatus;
 import org.folio.domain.dto.TransactionStatusResponse;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "dcb", url = "${folio.okapi-url}", configuration = FeignClientConfiguration.class)
@@ -14,5 +17,12 @@ public interface DcbClient {
   @PostMapping("/ecs-tlr-transactions/{dcbTransactionId}")
   TransactionStatusResponse createDcbTransaction(@PathVariable String dcbTransactionId,
     @RequestBody DcbTransaction dcbTransaction);
+
+  @GetMapping("/transactions/{dcbTransactionId}/status")
+  TransactionStatusResponse getDcbTransactionStatus(@PathVariable String dcbTransactionId);
+
+  @PutMapping("/transactions/{dcbTransactionId}/status")
+  TransactionStatusResponse changeDcbTransactionStatus(@PathVariable String dcbTransactionId,
+    @RequestBody TransactionStatus newStatus);
 
 }
--- src/test/java/org/folio/client/DcbClientTest.java
+++ src/test/java/org/folio/client/DcbClientTest.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 
 import org.folio.client.feign.DcbClient;
 import org.folio.domain.dto.DcbTransaction;
+import org.folio.domain.dto.TransactionStatus;
 import org.folio.domain.dto.TransactionStatusResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,5 +40,45 @@ class DcbClientTest {
     assertNotNull(response);
     assertEquals(TransactionStatusResponse.RoleEnum.BORROWER, response.getRole());
     assertEquals(requestId, response.getRequestId());
+  }
+
+  @Test
+  void canGetDcbTransactionStatus() {
+    String requestId = UUID.randomUUID().toString();
+    String transactionId = UUID.randomUUID().toString();
+    DcbTransaction dcbTransaction = new DcbTransaction()
+      .role(DcbTransaction.RoleEnum.BORROWER)
+      .requestId(requestId);
+    TransactionStatusResponse transactionStatusResponse = new TransactionStatusResponse()
+      .status(TransactionStatusResponse.StatusEnum.CANCELLED)
+      .message("test message")
+      .item(dcbTransaction.getItem())
+      .requestId(requestId);
+    when(dcbClient.getDcbTransactionStatus(transactionId)).thenReturn(transactionStatusResponse);
+    var response = dcbClient.getDcbTransactionStatus(transactionId);
+    assertNotNull(response);
+    assertEquals(TransactionStatusResponse.StatusEnum.CANCELLED, response.getStatus());
+  }
+
+  @Test
+  void canChangeDcbTransactionStatus() {
+    String requestId = UUID.randomUUID().toString();
+    String transactionId = UUID.randomUUID().toString();
+    TransactionStatus targetStatus = new TransactionStatus()
+      .status(TransactionStatus.StatusEnum.CANCELLED)
+      .message("test message");
+    DcbTransaction dcbTransaction = new DcbTransaction()
+      .role(DcbTransaction.RoleEnum.BORROWER)
+      .requestId(requestId);
+    TransactionStatusResponse transactionStatusResponse = new TransactionStatusResponse()
+      .status(TransactionStatusResponse.StatusEnum.CANCELLED)
+      .message("test message")
+      .item(dcbTransaction.getItem())
+      .requestId(requestId);
+    when(dcbClient.changeDcbTransactionStatus(transactionId, targetStatus))
+      .thenReturn(transactionStatusResponse);
+    var response = dcbClient.changeDcbTransactionStatus(transactionId, targetStatus);
+    assertNotNull(response);
+    assertEquals(TransactionStatusResponse.StatusEnum.CANCELLED, response.getStatus());
   }
 }
