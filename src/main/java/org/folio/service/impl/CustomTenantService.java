db4bc19de4c40d21c2fa4eec29f674a526a46423
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-03-19 15:52:10 +0200

    MODTLR-20 debug: create system user for tenant "college"

--- src/main/java/org/folio/service/impl/CustomTenantService.java
+++ src/main/java/org/folio/service/impl/CustomTenantService.java
@@ -4,8 +4,10 @@ import lombok.extern.log4j.Log4j2;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.PrepareSystemUserService;
+import org.folio.spring.service.SystemUserScopedExecutionService;
 import org.folio.spring.service.TenantService;
 import org.folio.tenant.domain.dto.TenantAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,8 @@ import org.springframework.stereotype.Service;
 @Primary
 public class CustomTenantService extends TenantService {
   private final PrepareSystemUserService systemUserService;
+  @Autowired
+  private SystemUserScopedExecutionService executionService;
 
   public CustomTenantService(JdbcTemplate jdbcTemplate, FolioExecutionContext context,
     FolioSpringLiquibase folioSpringLiquibase, PrepareSystemUserService systemUserService) {
@@ -28,6 +32,7 @@ public class CustomTenantService extends TenantService {
     log.debug("afterTenantUpdate:: parameters tenantAttributes: {}", () -> tenantAttributes);
     log.info("afterTenantUpdate:: start");
     systemUserService.setupSystemUser();
+    executionService.executeAsyncSystemUserScoped("college", systemUserService::setupSystemUser);
     log.info("afterTenantUpdate:: finished");
   }
 }
