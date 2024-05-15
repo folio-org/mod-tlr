922053f24faf15773de27076b7462a9aaaa924df
alexanderkurash <alexander.kurash@gmail.com>
2024-05-15 17:18:19 +0300

    MODTLR-25 Fix permission

--- src/test/java/org/folio/api/AllowedServicePointsApiTest.java
+++ src/test/java/org/folio/api/AllowedServicePointsApiTest.java
@@ -4,7 +4,10 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 
+import java.util.List;
+
 import org.apache.http.HttpStatus;
+import org.folio.domain.dto.AllowedServicePointsInner;
 import org.folio.domain.dto.AllowedServicePointsResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,8 +25,12 @@ public class AllowedServicePointsApiTest extends BaseIT {
   @Test
   void allowedServicePointCallProxiedToModCirculationEndpoint() {
     AllowedServicePointsResponse modCirculationMockedResponse = new AllowedServicePointsResponse();
-//    modCirculationMockedResponse.setHold();
-//    wireMockServer.stubFor(get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
-//      .willReturn(jsonResponse(mockSearchInstancesResponse, HttpStatus.SC_OK)));
+    modCirculationMockedResponse.setHold(List.of(new AllowedServicePointsInner(randomId(), "SP1"),
+      new AllowedServicePointsInner(randomId(), "SP2")));
+    modCirculationMockedResponse.setPage(List.of());
+    modCirculationMockedResponse.setRecall(List.of(new AllowedServicePointsInner(randomId(), "SP3")));
+
+    wireMockServer.stubFor(get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
+      .willReturn(jsonResponse(modCirculationMockedResponse, HttpStatus.SC_OK)));
   }
 }
