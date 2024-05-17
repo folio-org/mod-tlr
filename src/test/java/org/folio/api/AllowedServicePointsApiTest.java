eba9047cc82b0956acbb7b5101ba91aa398e1e7b
alexanderkurash <alexander.kurash@gmail.com>
2024-05-17 14:29:39 +0300

    MODTLR-25 Fix tests

--- src/test/java/org/folio/api/AllowedServicePointsApiTest.java
+++ src/test/java/org/folio/api/AllowedServicePointsApiTest.java
@@ -1,7 +1,6 @@
 package org.folio.api;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
@@ -16,12 +15,10 @@ import org.folio.domain.dto.AllowedServicePointsResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.github.tomakehurst.wiremock.stubbing.StubMapping;
-
 public class AllowedServicePointsApiTest extends BaseIT {
   private static final String ALLOWED_SERVICE_POINTS_URL = "/tlr/allowed-service-points";
   private static final String ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL =
-    "(.*)/circulation/requests/allowed-service-points(.*)";
+    "/circulation/requests/allowed-service-points(.*)";
 
   @BeforeEach
   public void beforeEach() {
@@ -33,18 +30,21 @@ public class AllowedServicePointsApiTest extends BaseIT {
     AllowedServicePointsResponse modCirculationMockedResponse = new AllowedServicePointsResponse();
     modCirculationMockedResponse.setHold(List.of(new AllowedServicePointsInner(randomId(), "SP1"),
       new AllowedServicePointsInner(randomId(), "SP2")));
-    modCirculationMockedResponse.setPage(List.of());
-    modCirculationMockedResponse.setRecall(List.of(new AllowedServicePointsInner(randomId(), "SP3")));
+    modCirculationMockedResponse.setPage(null);
+    modCirculationMockedResponse.setRecall(List.of(new AllowedServicePointsInner(randomId(),
+      "SP3")));
+
+    System.out.println(asJsonString(modCirculationMockedResponse));
 
     wireMockServer.stubFor(get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
-      .willReturn(jsonResponse(modCirculationMockedResponse, HttpStatus.SC_OK)));
+      .willReturn(jsonResponse(asJsonString(modCirculationMockedResponse), HttpStatus.SC_OK)));
 
     String requesterId = randomId();
     String instanceId = randomId();
-    doGet(ALLOWED_SERVICE_POINTS_URL + format("?requesterId=%s&instanceId=%s", requesterId,
-      instanceId))
-      .expectStatus().isEqualTo(200);
-//      .expectBody().json(asJsonString(modCirculationMockedResponse));
+    doGet(
+      ALLOWED_SERVICE_POINTS_URL + format("?requesterId=%s&instanceId=%s", requesterId, instanceId))
+      .expectStatus().isEqualTo(200)
+      .expectBody().json(asJsonString(modCirculationMockedResponse));
 
     wireMockServer.verify(getRequestedFor(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
       .withQueryParam("requesterId", equalTo(requesterId))
