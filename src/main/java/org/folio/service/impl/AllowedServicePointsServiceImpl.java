46e2d492942653b93c232dd4c7412db9a75961d5
alexanderkurash <alexander.kurash@gmail.com>
2024-06-11 15:00:34 +0300

    MODTLR-26 Call data tenants

--- src/main/java/org/folio/service/impl/AllowedServicePointsServiceImpl.java
+++ src/main/java/org/folio/service/impl/AllowedServicePointsServiceImpl.java
@@ -1,9 +1,18 @@
 package org.folio.service.impl;
 
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Stream;
+
 import org.folio.client.feign.CirculationClient;
+import org.folio.client.feign.SearchClient;
+import org.folio.domain.dto.AllowedServicePointsInner;
 import org.folio.domain.dto.AllowedServicePointsResponse;
+import org.folio.domain.dto.Instance;
+import org.folio.domain.dto.Item;
 import org.folio.domain.dto.RequestOperation;
 import org.folio.service.AllowedServicePointsService;
+import org.folio.spring.service.SystemUserScopedExecutionService;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -14,17 +23,55 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class AllowedServicePointsServiceImpl implements AllowedServicePointsService {
 
+  private final SearchClient searchClient;
   private final CirculationClient circulationClient;
+  private final SystemUserScopedExecutionService executionService;
 
   @Override
   public AllowedServicePointsResponse getAllowedServicePoints(RequestOperation operation,
     String requesterId, String instanceId) {
 
-    log.debug("getAllowedServicePoints:: params: requesterId={}, instanceId={}", requesterId,
-      instanceId);
+    log.debug("getAllowedServicePoints:: params: operation={}, requesterId={}, instanceId={}",
+      operation, requesterId, instanceId);
 
-    return circulationClient.allowedServicePoints(requesterId, instanceId,
-      operation.toString().toLowerCase(), true);
+    var searchInstancesResponse = searchClient.searchInstance(instanceId);
+    var availableForRequesting = searchInstancesResponse.getInstances().stream()
+      .map(Instance::getItems)
+      .flatMap(Collection::stream)
+      .map(Item::getTenantId)
+      .filter(Objects::nonNull)
+      .distinct()
+      .anyMatch(tenantId -> checkAvailability(tenantId, operation, requesterId, instanceId));
+
+    if (availableForRequesting) {
+      log.info("getAllowedServicePoints:: Available for requesting, proxying call");
+      return circulationClient.allowedServicePoints(requesterId, instanceId,
+        operation.toString().toLowerCase(), true);
+    } else {
+      log.info("getAllowedServicePoints:: Not available for requesting, returning empty result");
+      return new AllowedServicePointsResponse();
+    }
+  }
+
+  private boolean checkAvailability(String tenantId, RequestOperation operation,
+    String requesterId, String instanceId) {
+
+    log.debug("checkAvailability:: params: tenantId={}, operation={}, requesterId={}, instanceId={}",
+      tenantId, operation, requesterId, instanceId);
+
+    var allowedServicePointsResponse = executionService.executeSystemUserScoped(tenantId,
+      () -> circulationClient.allowedServicePoints(requesterId, instanceId,
+        operation.toString().toLowerCase(), true));
+
+    var availabilityCheckResult = Stream.of(allowedServicePointsResponse.getHold(),
+        allowedServicePointsResponse.getPage(), allowedServicePointsResponse.getRecall())
+      .filter(Objects::nonNull)
+      .flatMap(Collection::stream)
+      .anyMatch(AllowedServicePointsInner::getEcsRequestRouting);
+
+    log.info("checkAvailability:: result: {}", availabilityCheckResult);
+
+    return availabilityCheckResult;
   }
 
 }
--- src/test/java/org/folio/api/AllowedServicePointsApiTest.java
+++ src/test/java/org/folio/api/AllowedServicePointsApiTest.java
@@ -7,11 +7,16 @@ import static com.github.tomakehurst.wiremock.client.WireMock.jsonResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
 import static java.lang.String.format;
 
+import java.util.List;
 import java.util.Set;
 
 import org.apache.http.HttpStatus;
 import org.folio.domain.dto.AllowedServicePointsInner;
 import org.folio.domain.dto.AllowedServicePointsResponse;
+import org.folio.domain.dto.Holding;
+import org.folio.domain.dto.Instance;
+import org.folio.domain.dto.Item;
+import org.folio.domain.dto.SearchInstancesResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +24,9 @@ class AllowedServicePointsApiTest extends BaseIT {
   private static final String ALLOWED_SERVICE_POINTS_URL = "/tlr/allowed-service-points";
   private static final String ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL =
     "/circulation/requests/allowed-service-points(.*)";
+  private static final String SEARCH_INSTANCES_URL =
+    "/search/instances(.*)";
+  private static final String TENANT_HEADER = "x-okapi-tenant";
 
   @BeforeEach
   public void beforeEach() {
@@ -26,17 +34,63 @@ class AllowedServicePointsApiTest extends BaseIT {
   }
 
   @Test
-  void allowedServicePointCallProxiedToModCirculationEndpoint() {
-    AllowedServicePointsResponse modCirculationMockedResponse = new AllowedServicePointsResponse();
-    modCirculationMockedResponse.setHold(Set.of(
-      new AllowedServicePointsInner().id(randomId()).name("SP1"),
-      new AllowedServicePointsInner().id(randomId()).name("SP2")));
-    modCirculationMockedResponse.setPage(null);
-    modCirculationMockedResponse.setRecall(Set.of(
-      new AllowedServicePointsInner().id(randomId()).name("SP3")));
+  void allowedServicePointReturnsEmptyResultWhenNoRoutingSpInResponsesFromDataTenants() {
+    var item1 = new Item(List.of());
+    item1.setTenantId(TENANT_ID_UNIVERSITY);
+
+    var item2 = new Item(List.of());
+    item2.setTenantId(TENANT_ID_COLLEGE);
+
+    var searchInstancesResponse = new SearchInstancesResponse();
+    searchInstancesResponse.setTotalRecords(1);
+    searchInstancesResponse.setInstances(List.of(new Instance(List.of(), List.of(),
+      List.of(item1, item2),
+      List.of(new Holding(List.of(), List.of()))
+    )));
+
+    wireMockServer.stubFor(get(urlMatching(SEARCH_INSTANCES_URL))
+      .withHeader(TENANT_HEADER, equalTo(TENANT_ID_CONSORTIUM))
+      .willReturn(jsonResponse(asJsonString(searchInstancesResponse), HttpStatus.SC_OK)));
+
+    var allowedSpResponseConsortium = new AllowedServicePointsResponse();
+    allowedSpResponseConsortium.setHold(Set.of(
+      buildAllowedServicePoint("SP_consortium_1", null),
+      buildAllowedServicePoint("SP_consortium_2", null)));
+    allowedSpResponseConsortium.setPage(null);
+    allowedSpResponseConsortium.setRecall(Set.of(
+      buildAllowedServicePoint("SP_consortium_3", null)));
+
+    var allowedSpResponseUniversity = new AllowedServicePointsResponse();
+    allowedSpResponseUniversity.setHold(Set.of(
+      buildAllowedServicePoint("SP_university_1", false),
+      buildAllowedServicePoint("SP_university_2", false)));
+    allowedSpResponseUniversity.setPage(null);
+    allowedSpResponseUniversity.setRecall(null);
+
+    var allowedSpResponseCollege = new AllowedServicePointsResponse();
+    allowedSpResponseCollege.setHold(null);
+    allowedSpResponseCollege.setPage(Set.of(
+      buildAllowedServicePoint("SP_college_1", false)));
+    allowedSpResponseCollege.setRecall(null);
+
+    var allowedSpResponseCollegeWithRouting = new AllowedServicePointsResponse();
+    allowedSpResponseCollegeWithRouting.setHold(null);
+    allowedSpResponseCollegeWithRouting.setPage(Set.of(
+      buildAllowedServicePoint("SP_college_1", true)));
+    allowedSpResponseCollegeWithRouting.setRecall(null);
 
     wireMockServer.stubFor(get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
-      .willReturn(jsonResponse(asJsonString(modCirculationMockedResponse), HttpStatus.SC_OK)));
+      .withHeader(TENANT_HEADER, equalTo(TENANT_ID_CONSORTIUM))
+      .willReturn(jsonResponse(asJsonString(allowedSpResponseConsortium), HttpStatus.SC_OK)));
+
+    wireMockServer.stubFor(get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
+      .withHeader(TENANT_HEADER, equalTo(TENANT_ID_UNIVERSITY))
+      .willReturn(jsonResponse(asJsonString(allowedSpResponseUniversity), HttpStatus.SC_OK)));
+
+    var collegeStubMapping = wireMockServer.stubFor(
+      get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
+        .withHeader(TENANT_HEADER, equalTo(TENANT_ID_COLLEGE))
+        .willReturn(jsonResponse(asJsonString(allowedSpResponseCollege), HttpStatus.SC_OK)));
 
     String requesterId = randomId();
     String instanceId = randomId();
@@ -44,12 +98,33 @@ class AllowedServicePointsApiTest extends BaseIT {
       ALLOWED_SERVICE_POINTS_URL + format("?operation=create&requesterId=%s&instanceId=%s",
         requesterId, instanceId))
       .expectStatus().isEqualTo(200)
-      .expectBody().json(asJsonString(modCirculationMockedResponse));
+      .expectBody().json("{}");
+
+    wireMockServer.removeStub(collegeStubMapping);
+    wireMockServer.stubFor(get(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
+      .withHeader(TENANT_HEADER, equalTo(TENANT_ID_COLLEGE))
+      .willReturn(jsonResponse(asJsonString(allowedSpResponseCollegeWithRouting),
+        HttpStatus.SC_OK)));
+
+    doGet(
+      ALLOWED_SERVICE_POINTS_URL + format("?operation=create&requesterId=%s&instanceId=%s",
+        requesterId, instanceId))
+      .expectStatus().isEqualTo(200)
+      .expectBody().json(asJsonString(allowedSpResponseConsortium));
 
     wireMockServer.verify(getRequestedFor(urlMatching(ALLOWED_SERVICE_POINTS_MOD_CIRCULATION_URL))
       .withQueryParam("requesterId", equalTo(requesterId))
       .withQueryParam("instanceId", equalTo(instanceId))
       .withQueryParam("operation", equalTo("create"))
       .withQueryParam("useStubItem", equalTo("true")));
+  }
+
+  private AllowedServicePointsInner buildAllowedServicePoint(String name,
+    Boolean ecsRequestRouting) {
+
+    return new AllowedServicePointsInner()
+      .id(randomId())
+      .name(name)
+      .ecsRequestRouting(ecsRequestRouting);
   }
 }
