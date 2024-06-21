de462377efa7a49024f48e0e11e9ba8b13f52d26
Roman_Barannyk <roman_barannyk@epam.com>
2024-06-21 23:23:00 +0300

    MODTLR-48 change name for feign client

--- src/main/java/org/folio/client/feign/UserTenantsClient.java
+++ src/main/java/org/folio/client/feign/UserTenantsClient.java
@@ -3,12 +3,13 @@ package org.folio.client.feign;
 import org.folio.domain.dto.UserTenantCollection;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "userTenants", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "user-tenants", configuration = FeignClientConfiguration.class)
 public interface UserTenantsClient {
 
-  @GetMapping("/user-tenants")
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   UserTenantCollection getUserTenants(@RequestParam(name = "limit", required = false) Integer limit);
 }
