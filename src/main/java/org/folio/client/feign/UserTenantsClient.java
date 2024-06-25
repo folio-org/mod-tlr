1a40d4bbba8511751fdc7ac781b17ba17d936dee
Roman_Barannyk <roman_barannyk@epam.com>
2024-06-25 14:12:55 +0300

    MODTLR-48 update UserTenantsClient

--- src/main/java/org/folio/client/feign/UserTenantsClient.java
+++ src/main/java/org/folio/client/feign/UserTenantsClient.java
@@ -4,11 +4,15 @@ import org.folio.domain.dto.UserTenantCollection;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-tenants", configuration = FeignClientConfiguration.class)
 public interface UserTenantsClient {
 
-  @GetMapping(value = "/user-tenants")
-  UserTenantCollection getUserTenants(@RequestParam(name = "limit", required = false) Integer limit);
+//  @GetMapping(value = "/user-tenants")
+//  UserTenantCollection getUserTenants(@RequestParam(name = "limit", required = false) Integer limit);
+
+  @GetMapping(value = "?limit={limit}")
+  UserTenantCollection getUserTenants(@PathVariable("limit") Integer limit);
 }
