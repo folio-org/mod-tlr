db632c1e256788cca5b3669ea098e6c1e4110888
Roman_Barannyk <roman_barannyk@epam.com>
2024-06-25 13:40:21 +0300

    MODTLR-48 remove mediaType from client

--- src/main/java/org/folio/client/feign/UserTenantsClient.java
+++ src/main/java/org/folio/client/feign/UserTenantsClient.java
@@ -3,13 +3,12 @@ package org.folio.client.feign;
 import org.folio.domain.dto.UserTenantCollection;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-tenants", configuration = FeignClientConfiguration.class)
 public interface UserTenantsClient {
 
-  @GetMapping(value = "/user-tenants", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/user-tenants")
   UserTenantCollection getUserTenants(@RequestParam(name = "limit", required = false) Integer limit);
 }
