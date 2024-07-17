191de8c72e876a666f97515f063970b2f4a83dfc
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-07-17 18:25:39 +0300

    MODTLR-41 Attempt to fix request storage client

--- src/main/java/org/folio/client/feign/RequestStorageClient.java
+++ src/main/java/org/folio/client/feign/RequestStorageClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "request-storage", url = "/request-storage/requests", configuration = FeignClientConfiguration.class)
+@FeignClient(name = "request-storage", url = "request-storage/requests", configuration = FeignClientConfiguration.class)
 public interface RequestStorageClient {
 
   @GetMapping("/{requestId}")
