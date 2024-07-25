c01673ad9ba90de5f5e10ae145160c0bb44a4f59
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-07-25 17:31:10 +0300

    MODTLR-51 Remove redundant check

--- src/main/java/org/folio/service/DcbService.java
+++ src/main/java/org/folio/service/DcbService.java
@@ -9,7 +9,7 @@ import org.folio.domain.entity.EcsTlrEntity;
 
 public interface DcbService {
   void createLendingTransaction(EcsTlrEntity ecsTlr);
-  void createBorrowingTransaction(EcsTlrEntity ecsTlr, Request primaryRequest);
+  void createBorrowingTransaction(EcsTlrEntity ecsTlr, Request request);
   TransactionStatusResponse getTransactionStatus(UUID transactionId, String tenantId);
   TransactionStatusResponse updateTransactionStatus(UUID transactionId,
     TransactionStatus.StatusEnum newStatus, String tenantId);
