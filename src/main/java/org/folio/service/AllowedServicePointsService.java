e9af804e1f6eb5f44c0cae12eff16fca14226715
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-08-23 13:34:59 +0300

    MODTLR-56 Remove redundant import

--- src/main/java/org/folio/service/AllowedServicePointsService.java
+++ src/main/java/org/folio/service/AllowedServicePointsService.java
@@ -2,7 +2,6 @@ package org.folio.service;
 
 import org.folio.domain.dto.AllowedServicePointsRequest;
 import org.folio.domain.dto.AllowedServicePointsResponse;
-import org.folio.domain.dto.RequestOperation;
 
 public interface AllowedServicePointsService {
 
