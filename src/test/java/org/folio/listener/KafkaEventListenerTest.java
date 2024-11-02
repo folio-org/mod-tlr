0670231871894f383e48834c01c9c75da8eda17c
alexanderkurash <alexander.kurash@gmail.com>
2024-11-02 16:38:56 +0200

    MODTLR-69 Add a test

---
+++ src/test/java/org/folio/listener/KafkaEventListenerTest.java
@@ -0,0 +1,40 @@
+package org.folio.listener;
+
+import static org.folio.spring.integration.XOkapiHeaders.TENANT;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+
+import java.util.Map;
+
+import org.folio.listener.kafka.KafkaEventListener;
+import org.folio.service.impl.RequestBatchUpdateEventHandler;
+import org.folio.service.impl.RequestEventHandler;
+import org.folio.service.impl.UserGroupEventHandler;
+import org.folio.spring.service.SystemUserScopedExecutionService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.messaging.MessageHeaders;
+
+@ExtendWith(MockitoExtension.class)
+public class KafkaEventListenerTest {
+  @Mock
+  RequestEventHandler requestEventHandler;
+  @Mock
+  RequestBatchUpdateEventHandler requestBatchEventHandler;
+  @Mock
+  SystemUserScopedExecutionService systemUserScopedExecutionService;
+  @Mock
+  UserGroupEventHandler userGroupEventHandler;
+
+  @Test
+  void shouldHandleExceptionInEventHandler() {
+    doThrow(new NullPointerException("NPE")).when(systemUserScopedExecutionService)
+      .executeAsyncSystemUserScoped(any(), any());
+    KafkaEventListener kafkaEventListener = new KafkaEventListener(requestEventHandler,
+      requestBatchEventHandler, systemUserScopedExecutionService, userGroupEventHandler);
+    kafkaEventListener.handleRequestEvent("{}",
+      new MessageHeaders(Map.of(TENANT, "default".getBytes())));
+  }
+}
