43d5b5762e2928d669f0555663a939e032ebd545
Alexander Kurash <alexander.kurash@gmail.com>
2024-11-28 23:25:37 +0200

    MODTLR-104 Remove okapi token parsing (#82)
    * MODTLR-104 Remove okapi token parsing
    
    * MODTLR-104 Fix tests
    
    * MODTLR-104 Increase coverage
    
    * MODTLR-104 Remove unused import

--- src/main/java/org/folio/service/TenantService.java
+++ src/main/java/org/folio/service/TenantService.java
@@ -1,12 +1,11 @@
 package org.folio.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.folio.domain.entity.EcsTlrEntity;
 
 public interface TenantService {
-  Optional<String> getBorrowingTenant(EcsTlrEntity ecsTlr);
+  String getBorrowingTenant(EcsTlrEntity ecsTlr);
 
   List<String> getLendingTenants(EcsTlrEntity ecsTlr);
 }
