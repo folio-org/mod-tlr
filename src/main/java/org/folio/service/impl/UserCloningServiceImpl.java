180d45a7c774d6ee228ab40acaa53e2142eb505d
alexanderkurash <alexander.kurash@gmail.com>
2024-12-11 20:28:26 +0200

    MODTLR-116 Copy Secure Patron name

--- src/main/java/org/folio/service/impl/UserCloningServiceImpl.java
+++ src/main/java/org/folio/service/impl/UserCloningServiceImpl.java
@@ -1,6 +1,7 @@
 package org.folio.service.impl;
 
 import org.folio.domain.dto.User;
+import org.folio.domain.dto.UserPersonal;
 import org.folio.domain.dto.UserType;
 import org.folio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,17 @@ public class UserCloningServiceImpl extends CloningServiceImpl<User> {
       .type(UserType.SHADOW.getValue())
       .barcode(original.getBarcode())
       .active(true);
+
+    // TODO: Remove hardcoded Secure Patron name. mod-tlr shouldn't know about secure requests,
+    //  but there should be a mechanism to let it know that the user's name should also be copied
+    String firstName = original.getPersonal().getFirstName();
+    String lastName = original.getPersonal().getLastName();
+    if ("Secure".equals(firstName) && "Patron".equals(lastName)) {
+      clone.personal(new UserPersonal()
+        .firstName(firstName)
+        .lastName(lastName));
+    }
+
     log.debug("buildClone:: result: {}", () -> clone);
     return clone;
   }
