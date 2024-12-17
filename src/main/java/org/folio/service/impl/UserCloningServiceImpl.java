f130d72c8a730a4ef9c7c710c573d0c482e8ed20
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-12-17 12:06:45 +0200

    Clone user name

--- src/main/java/org/folio/service/impl/UserCloningServiceImpl.java
+++ src/main/java/org/folio/service/impl/UserCloningServiceImpl.java
@@ -1,6 +1,7 @@
 package org.folio.service.impl;
 
 import org.folio.domain.dto.User;
+import org.folio.domain.dto.UserPersonal;
 import org.folio.domain.dto.UserType;
 import org.folio.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,14 @@ public class UserCloningServiceImpl extends CloningServiceImpl<User> {
       .type(UserType.SHADOW.getValue())
       .barcode(original.getBarcode())
       .active(true);
+
+    UserPersonal personal = original.getPersonal();
+    if (personal != null) {
+      clone.personal(new UserPersonal()
+        .firstName(personal.getFirstName())
+        .lastName(personal.getLastName())
+        .middleName(personal.getMiddleName()));
+    }
     log.debug("buildClone:: result: {}", () -> clone);
     return clone;
   }
