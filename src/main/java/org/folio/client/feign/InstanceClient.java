376a00eeaba207d131b715dd40889e10cd73c211
OleksandrVidinieiev <56632770+OleksandrVidinieiev@users.noreply.github.com>
2025-02-18 09:35:08 +0000

    MODTLR-141: Create shadow instance in primary request tenant (#104)
    * MODTLR-141 Create shadow instance in primary request tenant
    
    * MODTLR-141 Replace InventoryItem with Item
    
    * MODTLR-141 Share instance through central tenant
    
    * MODTLR-141 Fix TenantContext constructor
    
    * MODTLR-141 Call instance sharing API in central tenant without system user
    
    * MODTLR-141 Call instance sharing API in central tenant without system user
    
    * MODTLR-141 Log and throw sharing error
    
    * MODTLR-141 Call instance sharing API in target tenant
    
    * MODTLR-141 Call instance sharing API in target tenant
    
    * MODTLR-141 Refactoring
    
    * MODTLR-141 Fix build
    
    * MODTLR-141 Extend test
    
    * MODTLR-141 Remove redundant import
    
    * MODTLR-141 Fix ecs-tlr.yaml
    
    * MODTLR-141 Tests for ConsortiumService
    
    * MODTLR-141 Refactoring ConsortiumService
    
    * MODTLR-141 Refactoring
    
    * MODTLR-141 Unit test for instance sharing failure
    
    * MODTLR-141 Remove instance sharing permission from module permissions
    
    (cherry picked from commit fd6a42aeed412e79cb3275f3db21c33dfcadd131)

--- src/main/java/org/folio/client/feign/InstanceClient.java
+++ src/main/java/org/folio/client/feign/InstanceClient.java
@@ -1,7 +1,7 @@
 package org.folio.client.feign;
 
+import org.folio.domain.dto.Instance;
 import org.folio.domain.dto.Instances;
-import org.folio.domain.dto.InventoryInstance;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface InstanceClient extends GetByQueryClient<Instances> {
 
   @GetMapping("/{id}")
-  InventoryInstance get(@PathVariable String id);
+  Instance get(@PathVariable String id);
 
 }
---
+++ src/main/java/org/folio/service/ConsortiumService.java
@@ -0,0 +1,9 @@
+package org.folio.service;
+
+public interface ConsortiumService {
+  String getCurrentTenantId();
+  String getCurrentConsortiumId();
+  String getCentralTenantId();
+  boolean isCurrentTenantCentral();
+  boolean isCentralTenant(String tenantId);
+}
