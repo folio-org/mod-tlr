5f07067e18620a78d0ee66c097111bef40b3d2aa
Roman_Barannyk <roman_barannyk@epam.com>
2025-03-07 21:56:00 +0200

    MODTLR-152 fix code smells

--- src/main/java/org/folio/client/feign/CirculationClient.java
+++ src/main/java/org/folio/client/feign/CirculationClient.java
@@ -1,14 +1,11 @@
 package org.folio.client.feign;
 
 import org.folio.domain.dto.AllowedServicePointsResponse;
-import org.folio.domain.dto.CheckOutRequest;
-import org.folio.domain.dto.CheckOutResponse;
 import org.folio.domain.dto.Request;
 import org.folio.spring.config.FeignClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "circulation", url = "circulation", configuration = FeignClientConfiguration.class)
