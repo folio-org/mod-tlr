899e0b4c8cdaa5708d1c476d700598f008ca02fe
alexanderkurash <alexander.kurash@gmail.com>
2025-07-31 21:37:01 +0300

    MODTLR-220 Add and refactor tests

--- src/test/java/org/folio/api/DeclareItemLostApiTest.java
+++ src/test/java/org/folio/api/DeclareItemLostApiTest.java
@@ -1,188 +1,99 @@
 package org.folio.api;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
-import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.noContent;
-import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static org.folio.spring.integration.XOkapiHeaders.TENANT;
 
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Date;
-import java.util.UUID;
 
-import org.folio.client.feign.CirculationClient;
 import org.folio.domain.dto.CirculationDeclareItemLostRequest;
 import org.folio.domain.dto.DeclareItemLostRequest;
-import org.folio.domain.dto.Loan;
-import org.folio.domain.dto.Loans;
-import org.folio.domain.dto.Metadata;
-import org.folio.domain.dto.Request;
-import org.folio.domain.dto.Requests;
-import org.folio.domain.entity.EcsTlrEntity;
 import org.folio.repository.EcsTlrRepository;
+import org.folio.support.MockDataUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-class DeclareItemLostApiTest extends BaseIT {
-
-  private static final UUID SERVICE_POINT_ID = UUID.randomUUID();
-  private static final UUID LOCAL_TENANT_LOAN_ID = UUID.randomUUID();
-  private static final UUID LENDING_TENANT_LOAN_ID = UUID.randomUUID();
-  private static final UUID USER_ID = UUID.randomUUID();
-  private static final UUID ITEM_ID = UUID.randomUUID();
-  private static final UUID ECS_REQUEST_ID = UUID.randomUUID();
+class DeclareItemLostApiTest extends LoanActionApiTest {
 
   private static final String DECLARE_ITEM_LOST_URL = "/tlr/loans/declare-item-lost";
-  private static final String LOAN_STORAGE_URL = "/loan-storage/loans";
-  private static final String REQUEST_STORAGE_URL = "/request-storage/requests";
   private static final String CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE =
     "/circulation/loans/%s/declare-item-lost";
 
-  private static final String DECLARE_ITEM_LOST_COMMENT = "Test comment";
-
   @Autowired
   private EcsTlrRepository ecsTlrRepository;
-  @Autowired private CirculationClient circulationClient;
+
+  @BeforeEach
+  void beforeEach() {
+    wireMockServer.resetAll();
+    ecsTlrRepository.deleteAll();
+  }
 
   @Test
-  void declareItemLost() {
+  void declareItemLostByLoanId() {
     ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
     Date loanCreationDate = Date.from(now.toInstant());
-    Date requestUpdateDate = Date.from(now.minusSeconds(30).toInstant());
     Date declareItemLostDate = new Date();
 
-    // mock local loan
+    setupLocalLoanMock(loanCreationDate, LOCAL_TENANT_LOAN_ID.toString());
+    setupLendingTenantLoanMock(loanCreationDate);
+    setupEcsRequestMock(Date.from(now.minusSeconds(30).toInstant()));
+    mockEcsTlrEntity();
+    setupCirculationMocks(declareItemLostDate);
 
-    Loan mockLocalLoan = new Loan()
-      .id(LOCAL_TENANT_LOAN_ID.toString())
-      .userId(USER_ID.toString())
-      .itemId(ITEM_ID.toString())
-      .metadata(new Metadata().createdDate(loanCreationDate));
-
-    wireMockServer.stubFor(get(urlEqualTo(LOAN_STORAGE_URL + "/" + LOCAL_TENANT_LOAN_ID))
-      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .willReturn(okJson(asJsonString(mockLocalLoan))));
-
-    // mock loan in lending tenant
-
-    Loan mockLendingTenantLoan = new Loan()
-      .id(LENDING_TENANT_LOAN_ID.toString())
-      .userId(USER_ID.toString())
-      .itemId(ITEM_ID.toString())
-      .metadata(new Metadata().createdDate(loanCreationDate));
-
-    wireMockServer.stubFor(get(urlPathEqualTo(LOAN_STORAGE_URL))
-      .withHeader(TENANT, equalTo(TENANT_ID_COLLEGE))
-      .willReturn(okJson(asJsonString(new Loans().addLoansItem(mockLendingTenantLoan)))));
-
-    // mock local ECS request
-
-    Request mockEcsRequest = new Request()
-      .id(ECS_REQUEST_ID.toString())
-      .ecsRequestPhase(Request.EcsRequestPhaseEnum.PRIMARY)
-      .requesterId(USER_ID.toString())
-      .itemId(ITEM_ID.toString())
-      .status(Request.StatusEnum.CLOSED_FILLED)
-      .metadata(new Metadata().updatedDate(requestUpdateDate));
-
-    wireMockServer.stubFor(get(urlPathEqualTo(REQUEST_STORAGE_URL))
-      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .willReturn(okJson(asJsonString(new Requests().addRequestsItem(mockEcsRequest)))));
-
-    // mock ECS TLR
-
-    EcsTlrEntity ecsTlr = new EcsTlrEntity();
-    ecsTlr.setItemId(ITEM_ID);
-    ecsTlr.setRequesterId(USER_ID);
-    ecsTlr.setPrimaryRequestId(ECS_REQUEST_ID);
-    ecsTlr.setSecondaryRequestId(ECS_REQUEST_ID);
-    ecsTlr.setPrimaryRequestTenantId(TENANT_ID_CONSORTIUM);
-    ecsTlr.setSecondaryRequestTenantId(TENANT_ID_COLLEGE);
-    ecsTlrRepository.save(ecsTlr);
-
-    // mock declare item lost in local circulation
-
-    CirculationDeclareItemLostRequest expectedCirculationRequestInLocalTenant =
-      new CirculationDeclareItemLostRequest()
-        .servicePointId(SERVICE_POINT_ID)
-        .declaredLostDateTime(declareItemLostDate)
-        .comment(DECLARE_ITEM_LOST_COMMENT);
-
-    wireMockServer.stubFor(post(urlEqualTo(
-      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LOCAL_TENANT_LOAN_ID.toString())))
-      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .withRequestBody(equalToJson(asJsonString(expectedCirculationRequestInLocalTenant)))
-      .willReturn(noContent()));
-
-    // mock declare item lost in lending tenant circulation
-
-    CirculationDeclareItemLostRequest expectedCirculationRequestInLendingTenant =
-      new CirculationDeclareItemLostRequest()
-        .servicePointId(SERVICE_POINT_ID)
-        .declaredLostDateTime(declareItemLostDate)
-        .comment(DECLARE_ITEM_LOST_COMMENT);
-
-    wireMockServer.stubFor(post(urlEqualTo(
-      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LENDING_TENANT_LOAN_ID.toString())))
-      .withHeader(TENANT, equalTo(TENANT_ID_COLLEGE))
-      .withRequestBody(equalToJson(asJsonString(expectedCirculationRequestInLendingTenant)))
-      .willReturn(noContent()));
-
-    // declare item lost
-
-    DeclareItemLostRequest declareItemLostRequest = new DeclareItemLostRequest()
-      .loanId(LOCAL_TENANT_LOAN_ID)
-      .servicePointId(SERVICE_POINT_ID)
-      .declaredLostDateTime(declareItemLostDate)
-      .comment(DECLARE_ITEM_LOST_COMMENT);
+    DeclareItemLostRequest declareItemLostRequest = MockDataUtils.buildDeclareItemLostRequest(
+      LOCAL_TENANT_LOAN_ID, SERVICE_POINT_ID, declareItemLostDate, ACTION_COMMENT);
 
     declareItemLost(declareItemLostRequest)
       .expectStatus().isNoContent();
 
-    wireMockServer.verify(1, postRequestedFor(urlEqualTo(
-      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LOCAL_TENANT_LOAN_ID.toString())))
-      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .withRequestBody(equalToJson(asJsonString(expectedCirculationRequestInLocalTenant))));
+    verifyCirculationCalls(declareItemLostDate);
+  }
 
-    wireMockServer.verify(1, postRequestedFor(urlEqualTo(
-      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LENDING_TENANT_LOAN_ID.toString())))
-      .withHeader(TENANT, equalTo(TENANT_ID_COLLEGE))
-      .withRequestBody(equalToJson(asJsonString(expectedCirculationRequestInLendingTenant))));
+  @Test
+  void declareItemLostByUserIdAndItemId() {
+    ZonedDateTime now = ZonedDateTime.now(ZoneOffset.UTC);
+    Date loanCreationDate = Date.from(now.toInstant());
+    Date declareItemLostDate = new Date();
+
+    setupLocalLoanMockWithQuery(loanCreationDate);
+    setupLendingTenantLoanMock(loanCreationDate);
+    setupEcsRequestMock(Date.from(now.minusSeconds(30).toInstant()));
+    mockEcsTlrEntity();
+    setupCirculationMocks(declareItemLostDate);
+
+    DeclareItemLostRequest declareItemLostRequest = MockDataUtils.buildDeclareItemLostRequest(
+      USER_ID, ITEM_ID, SERVICE_POINT_ID, declareItemLostDate, ACTION_COMMENT);
+
+    declareItemLost(declareItemLostRequest)
+      .expectStatus().isNoContent();
+
+    verifyCirculationCalls(declareItemLostDate);
   }
 
   @ParameterizedTest
   @ValueSource(ints = { 400, 404, 422, 500 })
   void circulationApiErrorsAreForwarded(int circulationStatusCode) {
-    Loan mockLocalLoan = new Loan()
-      .id(LOCAL_TENANT_LOAN_ID.toString())
-      .userId(USER_ID.toString())
-      .itemId(ITEM_ID.toString())
-      .metadata(new Metadata().createdDate(new Date()));
+    setupLocalLoanMock(new Date(), LOCAL_TENANT_LOAN_ID.toString());
 
-    wireMockServer.stubFor(get(urlEqualTo(LOAN_STORAGE_URL + "/" + LOCAL_TENANT_LOAN_ID))
-      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .willReturn(okJson(asJsonString(mockLocalLoan))));
-
-    wireMockServer.stubFor(post(urlEqualTo(
-      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LOCAL_TENANT_LOAN_ID.toString())))
-      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
-      .willReturn(aResponse().withStatus(circulationStatusCode)));
+    mockErrorCode(
+      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LOCAL_TENANT_LOAN_ID.toString()),
+      circulationStatusCode);
 
     DeclareItemLostRequest request = new DeclareItemLostRequest()
       .loanId(LOCAL_TENANT_LOAN_ID)
       .servicePointId(SERVICE_POINT_ID)
       .declaredLostDateTime(new Date())
-      .comment(DECLARE_ITEM_LOST_COMMENT);
+      .comment(ACTION_COMMENT);
 
     declareItemLost(request)
       .expectStatus().isEqualTo(circulationStatusCode);
@@ -196,6 +107,47 @@ class DeclareItemLostApiTest extends BaseIT {
 
   private WebTestClient.ResponseSpec declareItemLost(DeclareItemLostRequest declareItemLostRequest) {
     return doPost(DECLARE_ITEM_LOST_URL, declareItemLostRequest);
+  }
+
+  private void setupCirculationMocks(Date declareItemLostDate) {
+    CirculationDeclareItemLostRequest expectedCirculationRequestInLocalTenant =
+      MockDataUtils.buildCirculationDeclareItemLostRequest(SERVICE_POINT_ID, declareItemLostDate,
+        ACTION_COMMENT);
+
+    wireMockServer.stubFor(post(urlEqualTo(
+      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LOCAL_TENANT_LOAN_ID.toString())))
+      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
+      .withRequestBody(equalToJson(asJsonString(expectedCirculationRequestInLocalTenant)))
+      .willReturn(noContent()));
+
+    CirculationDeclareItemLostRequest expectedCirculationRequestInLendingTenant =
+      MockDataUtils.buildCirculationDeclareItemLostRequest(SERVICE_POINT_ID, declareItemLostDate,
+        ACTION_COMMENT);
+
+    wireMockServer.stubFor(post(urlEqualTo(
+      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LENDING_TENANT_LOAN_ID.toString())))
+      .withHeader(TENANT, equalTo(TENANT_ID_COLLEGE))
+      .withRequestBody(equalToJson(asJsonString(expectedCirculationRequestInLendingTenant)))
+      .willReturn(noContent()));
+  }
+
+  private void verifyCirculationCalls(Date declareItemLostDate) {
+    CirculationDeclareItemLostRequest expectedLocalRequest =
+      MockDataUtils.buildCirculationDeclareItemLostRequest(SERVICE_POINT_ID, declareItemLostDate,
+        ACTION_COMMENT);
+    CirculationDeclareItemLostRequest expectedLendingRequest =
+      MockDataUtils.buildCirculationDeclareItemLostRequest(SERVICE_POINT_ID, declareItemLostDate,
+        ACTION_COMMENT);
+
+    wireMockServer.verify(1, postRequestedFor(urlEqualTo(
+      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LOCAL_TENANT_LOAN_ID.toString())))
+      .withHeader(TENANT, equalTo(TENANT_ID_CONSORTIUM))
+      .withRequestBody(equalToJson(asJsonString(expectedLocalRequest))));
+
+    wireMockServer.verify(1, postRequestedFor(urlEqualTo(
+      CIRCULATION_DECLARE_ITEM_LOST_URL_TEMPLATE.formatted(LENDING_TENANT_LOAN_ID.toString())))
+      .withHeader(TENANT, equalTo(TENANT_ID_COLLEGE))
+      .withRequestBody(equalToJson(asJsonString(expectedLendingRequest))));
   }
 
 }
