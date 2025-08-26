085d941a9690e75d2439c307090756453313dd71
alexanderkurash <alexander.kurash@gmail.com>
2025-08-26 19:35:40 +0300

    MODTLR-248 Use DCB location for circ items

---
+++ src/main/java/org/folio/domain/dto/DCBConstants.java
@@ -0,0 +1,9 @@
+package org.folio.domain.dto;
+
+public class DCBConstants {
+
+  private DCBConstants() {}
+
+  public static final String LOCATION_ID = "9d1b77e8-f02e-4b7f-b296-3f2042ddac54";
+
+}
