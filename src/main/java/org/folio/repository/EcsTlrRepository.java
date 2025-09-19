2cb51bf771b02b448197edd8ec9a5ccc6f0f44da
alexanderkurash <alexander.kurash@gmail.com>
2025-09-19 15:06:16 +0300

    MODTLR-251 Initial implementation

--- src/main/java/org/folio/repository/EcsTlrRepository.java
+++ src/main/java/org/folio/repository/EcsTlrRepository.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import org.folio.domain.entity.EcsTlrEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,4 +16,14 @@ public interface EcsTlrRepository extends JpaRepository<EcsTlrEntity, UUID> {
   Optional<EcsTlrEntity> findByInstanceId(UUID instanceId);
   List<EcsTlrEntity> findByPrimaryRequestIdIn(List<UUID> primaryRequestIds);
   List<EcsTlrEntity> findByItemId(UUID itemId);
+
+  @Query("""
+    SELECT ecsr from EcsTlrEntity ecsr
+    WHERE ecsr.requestLevel = 'Title'
+    AND ecsr.requesterId = ?1
+    AND ecsr.instanceId = ?2
+    AND ecsr.primaryRequestStatus IN ('Open - Not yet filled', 'Open - Awaiting pickup',
+        'Open - In transit', 'Open - Awaiting delivery')
+    """)
+  Optional<List<EcsTlrEntity>> findOpenRequests(UUID requesterId, UUID instanceId);
 }
